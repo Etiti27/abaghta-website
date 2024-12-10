1a3c4a7112cea65dec4c5013f221d5ee7678929c
etiti <christopherobinna27@gmail.com>
2024-12-10 20:14:10 +0100

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const now = new Date();
+
+
+
+const year = now.getFullYear();
+console.log(year);
+document.getElementById("dated").innerText = year;
+const names = document.getElementById("name").values;
+console.log(names);
+
+
+// document.getElementById('myForm2').addEventListener('submit', function(event) {
+//     // Prevent default form submission behavior
+//     event.preventDefault();
+//     const name = document.getElementById("name").value;
+//     const subject = document.getElementById("subject").value;
+//     const email = document.getElementById("email").value;
+//     const message = document.getElementById("message").value;
+//     console.log(name, subject, email, message);
+
+    
+
+
+
+
+
+// })
